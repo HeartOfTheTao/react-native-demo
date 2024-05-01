c923ea12f79520a17d19d824e1b41f129dd9916f
HeartOfTheTao <137022327+HeartOfTheTao@users.noreply.github.com>
2024-05-01 17:43:43 +0800

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,6 +1,6 @@
 #!/user/bin/env bash
   
-export PATH=$PATH:$HOME/Library/Android/sdk/cmdline-tools/laest/bin
+export PATH=$PATH:$HOME/Library/Android/sdk/cmdline-tools/latest/bin
   
 sdkmanager "platforms;android-23"
 sdkmanager "build-tools;23.0.0""build-tools;23.0.1""build-tools;23.0.2""build-tools;23.0.3"
