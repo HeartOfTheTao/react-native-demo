2ce65cbb4a8f807b9eac8611d2411522fef4384f
HeartOfTheTao <137022327+HeartOfTheTao@users.noreply.github.com>
2024-05-02 15:54:28 +0800

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,6 +1,7 @@
 #!/user/bin/env bash
 
 uname -m
+npm i install --save @react-native-community/cli-platform-android
 echo 'a' | softwareupdate --install-rosetta  
 export PATH=$PATH:$HOME/Library/Android/sdk/cmdline-tools/latest/bin
   
